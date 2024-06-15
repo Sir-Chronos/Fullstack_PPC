b1cb072c8d62f7d3a1e492af8b5d98d301b1e60a
eoKelvio <kelvinsc7@gmail.com>
2024-06-15 18:18:52 -0300

    chore: criacao dos endpoints no app.ts

--- src/app.ts
+++ src/app.ts
@@ -2,23 +2,48 @@ import express, { Request, Response } from "express";
 import sequelize from "./config/sequelize";
 
 import UserRouter from "./routes/UserRouter";
+import BibliographRouter from "./routes/BibliographRouter";
+import CurricularUnityRouter from "./routes/CurricularUnityRouter";
+import CurricularUnityBibliographRouter from "./routes/CurricularUnityBibliographRouter";
+import CurricularUnityKnowledgeRouter from "./routes/CurricularUnityKnowledgeRouter";
+import CurricularUnitySkillRouter from "./routes/CurricularUnitySkillRouter";
+import EvaluationPeriodsRouter from "./routes/EvaluationPeriodsRouter";
+import HolidaysRouter from "./routes/HolidaysRouter";
+import PPCRouter from "./routes/PPCRouter";
+import SkillRouter from "./routes/SkillRouter";
+import KnowledgeRouter from "./routes/KnowledgeRouter";
 
 const port = 8000;
 const app = express();
 
-app.use("/user", UserRouter)
+// Middleware for JSON handling
+app.use(express.json());
 
+// Registering all routes
+app.use("/user", UserRouter);
+app.use("/bibliograph", BibliographRouter);
+app.use("/curricular-unity", CurricularUnityRouter);
+app.use("/curricular-unity-bibliograph", CurricularUnityBibliographRouter);
+app.use("/curricular-unity-knowledge", CurricularUnityKnowledgeRouter);
+app.use("/curricular-unity-skill", CurricularUnitySkillRouter);
+app.use("/evaluation-periods", EvaluationPeriodsRouter);
+app.use("/holidays", HolidaysRouter);
+app.use("/ppc", PPCRouter);
+app.use("/skill", SkillRouter);
+app.use("/knowledge", KnowledgeRouter);
 
-app.use(express.json()); // Middleware for JSON handling
+// Root route
+app.get("/", (req: Request, res: Response) => {
+    res.send("Home page");
+});
 
+// Sync database and start server
 sequelize.sync().then(() => {
     app.listen(port, () => {
         console.log(`Now listening on port http://localhost:${port}`);
     });
-}).catch((err: Error) => { // Ensuring type safety with Error type
+}).catch((err: Error) => {
     console.error('Unable to connect to the database:', err);
 });
 
-app.get("/", (req: Request, res: Response) => {
-    res.send("Home page");
-});
+export default app;
